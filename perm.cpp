998a5cd67825caf0169de3aad6c195d89337a9de
shsharma <sharma.shantanu@gmail.com>
2014-12-16 15:23:35 +0530

    unique perms

--- perm.cpp
+++ perm.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include <map>
+
 
 using namespace std;
 void swap(char& a, char& b) {
@@ -9,13 +11,16 @@ a=b;
 b=t;
 }
 
-void perm(string s, int i) {
-    if(i == s.length())
-	cout<<"\n "<<s;
+void perm(string s, int i, map<string, int>& m) {
+    if(i == s.length()) {
+	//cout<<"\n "<<s;
+	//insert into map
+	m[s]++;
+    }
     else {
 	for(int j=i;j<s.length();++j) {
 	    swap(s[i], s[j]);
-	    perm(s, i+1);
+	    perm(s, i+1, m);
 	    swap(s[i], s[j]);
 	}
     }
@@ -23,7 +28,14 @@ void perm(string s, int i) {
 }
 
 int main() {
-string s("abc");
-perm(s,0);
+map<string, int> m;
+string s("aabb");
+perm(s,0, m);
+map<string, int>::iterator it = m.begin();
+while(it != m.end()) {
+    cout<<"\n** "<<(*it).first;
+    ++it;
+}
+cout<<"\n";
 return 0;
 }
